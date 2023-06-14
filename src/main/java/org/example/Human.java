35d31137495a2b83e0fd172c86e59037e0e3b8a6
Viktor Churikov <scanlave@gmail.com>
2023-06-14 21:29:14 +0300

    human add

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+    String firstName;
+    String lastName;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("Ivan", "Petrov");
+        System.out.println(human);
+    }
+}
