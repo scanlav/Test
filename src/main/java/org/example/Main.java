069fe475c7fa6936644e4555b1411d7348248e41
Viktor Churikov <scanlave@gmail.com>
2023-06-14 21:47:29 +0300

    human Petrov

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human("Ivan", "Ivanov");
+        Human human = new Human("Ivan", "Petrov");
         System.out.println(human);
     }
 }
